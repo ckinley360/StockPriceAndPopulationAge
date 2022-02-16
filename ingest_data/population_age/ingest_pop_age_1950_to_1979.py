ed90d0e567e00d35572ac5dfbed431b2d12d3491
Christian Kinley <ckinley360@users.noreply.github.com>
2022-02-15 16:10:38 -0800

    Update ingest_pop_age_1950_to_1979.py

--- ingest_data/population_age/ingest_pop_age_1950_to_1979.py
+++ ingest_data/population_age/ingest_pop_age_1950_to_1979.py
@@ -3,7 +3,10 @@ import pandas as pd
 def main():
     excelFile1950s = 'data_files\pe-11-1950s.xls'
 
-    df = pd.read_excel(excelFile1950s)
+    df = pd.read_excel(io=excelFile1950s, header=5, names=['Age','Population'], usecols=[0,1])
+    df = df[(df.Population.notnull()) & (df.Age != 'All ages')]
+    df = df.astype({'Population':int})
+    df = df.replace(to_replace='85+', value='85')
     print(df)
 
 if __name__ == '__main__':
